0cecd4b81da119deeae7444b77b69a831b357e1d
kbarnewolt <kelby-barnewolt@uiowa.edu>
2021-03-26 01:04:23 -0500

    edited html sheets

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -11,9 +11,18 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET'])
 def estimate():
     return render_template('estimate.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        radius = int(request.form['radius'])
+        height = int(request.form['height'])
+        area_top = 3.14 * (radius * radius)
+        print(area_top)
+    return render_template('estimate.html', myValue = area_top)
 
 if __name__ == '__main__':
     app.run(debug=True)
